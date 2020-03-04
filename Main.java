6e6b82e0e50fa0f501b7820e98de00e833a0d082
Tommy Gomez <tjk2132@columbia.edu>
2020-03-03 23:52:33 -0500

    added basic Main

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] iGotTha){
+        JFrame frame = new JFrame();
+        frame.setSize(1200, 800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return;
+        
+    }
+
+
+}
